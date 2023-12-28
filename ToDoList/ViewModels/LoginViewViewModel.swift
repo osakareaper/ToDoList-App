d3874f14f0dcf6c1d452486cb4cdf3410111955b
osakareaper <osakareaper@gmail.com>
2023-12-28 13:07:36 -0300

    feat: updates on login function

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -5,8 +5,8 @@
 //  Created by Marcus Osaka on 26/12/23.
 //
 
+import FirebaseAuth
 import Foundation
-
 
 class LoginViewViewModel: ObservableObject {
     @Published var email = ""
@@ -16,26 +16,32 @@ class LoginViewViewModel: ObservableObject {
     init() {}
     
     func login() {
+        // Validate
+        guard validate() else {
+            return
+        }
+        
+        // Try login
+        Auth.auth().signIn(withEmail: email, password: password)
+    }
+    
+    private func validate() -> Bool {
         errorMsg = ""
         
         // Fill all fields
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
               !password.trimmingCharacters(in: .whitespaces).isEmpty else {
             errorMsg = "Please fill in all fields"
-            return
+            return false
         }
         
         // Valid Email
         guard email.contains("@") && email.contains(".") else {
             errorMsg = "Enter a valid email"
-            return
+            return false
         }
         
-        // 
+        // Return true if doesn't have any errors
+        return true
     }
-    
-    func validate() {
-        
-    }
-    
 }
